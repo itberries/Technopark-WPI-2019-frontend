6b00c3fbf6c70117f94230df1a49b6f76571dc73
Anastasia Puchnina <puchnina.nastya@gmail.com>
2019-03-06 19:07:49 +0300

    fix conficts and eslint

--- src/panels/Games.js
+++ src/panels/Games.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 import { Panel, PanelHeader } from '@vkontakte/vkui';
 
 const Games = ({ id }) => (
-	<Panel id={id}>
-		<PanelHeader>Games</PanelHeader>
-	</Panel>
+  <Panel id={id}>
+    <PanelHeader>Games</PanelHeader>
+  </Panel>
 );
 
 Games.propTypes = {
-	id: PropTypes.string.isRequired
+  id: PropTypes.string.isRequired,
 };
 export default Games;
--- src/panels/Profile.js
+++ src/panels/Profile.js
@@ -2,21 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Panel, PanelHeader } from '@vkontakte/vkui';
 
-import User from "../components/User";
+import User from '../components/User';
 
 const Profile = ({ id, fetchedUser }) => (
-	<Panel id={id}>
-		<PanelHeader>Profile</PanelHeader>
-		<User fetchedUser={fetchedUser}/>
-	</Panel>
+  <Panel id={id}>
+    <PanelHeader>Profile</PanelHeader>
+    <User fetchedUser={fetchedUser} />
+  </Panel>
 );
 
 Profile.propTypes = {
-	id: PropTypes.string.isRequired,
-	fetchedUser: PropTypes.shape({
-		photo_200: PropTypes.string,
-		first_name: PropTypes.string,
-		last_name: PropTypes.string,
-	}),
+  id: PropTypes.string.isRequired,
+  fetchedUser: PropTypes.shape({
+    photo_200: PropTypes.string,
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+  }),
 };
+
+Profile.defaultProps = {
+  fetchedUser: PropTypes.shape({
+    photo_200: PropTypes.string,
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+  }),
+};
+
 export default Profile;
