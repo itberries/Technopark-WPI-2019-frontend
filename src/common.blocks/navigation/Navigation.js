af69e6814d3edcfa72ca03f6f956d55c0ad8fc99
Anastasia Puchnina <puchnina.nastya@gmail.com>
2019-03-09 00:36:48 +0300

    fixes (style, js)

--- src/common.blocks/navigation/Navigation.js
+++ src/common.blocks/navigation/Navigation.js
@@ -5,60 +5,61 @@ import {
 } from '@vkontakte/vkui';
 import './Navigation.css';
 
-const Navigation = ({ activePanel, panelsData, onPanelChange, fetchedUser }) => {
-
-  let tabbarItems = [];
-  let panels = [];
+const Navigation = ({
+  activePanel, panelsData, onPanelChange, fetchedUser,
+}) => {
+  const tabbarItems = [];
+  const panels = [];
   console.log(panelsData);
-  for (let i = 0; i < panelsData.length; i++) {
+
+  Object.values(panelsData).forEach((panelData) => {
     tabbarItems.push(
       <TabbarItem
         onClick={onPanelChange}
-        selected={activePanel === panelsData[i].name}
-        data-story={panelsData[i].name}
-        text={panelsData[i].text}
+        selected={activePanel === panelData.name}
+        data-story={panelData.name}
+        text={panelData.text}
       >
-        {(<img src={panelsData[i].icon} alt={panelsData[i].name + ' icon'} />)}
-      </TabbarItem>
+        {<img src={panelData.icon} alt={`${panelData.name} icon`} />}
+      </TabbarItem>,
     );
     panels.push(
-      <View id={panelsData[i].name} activePanel={panelsData[i].name}>
-        {React.createElement(panelsData[i].tag, {id: panelsData[i].name, fetchedUser: fetchedUser})}
-      </View>
+      <View id={panelData.name} activePanel={panelData.name}>
+        {React.createElement(panelData.tag, { id: panelData.name, fetchedUser })}
+      </View>,
     );
-  }
+  });
 
   return (
     <Epic
       // eslint-disable-next-line react/destructuring-assignment
       activeStory={activePanel}
-      tabbar={(
-        <Tabbar>{tabbarItems}</Tabbar>
-      )}
+      tabbar={<Tabbar>{tabbarItems}</Tabbar>}
     >
-      {panels}</Epic>
-    );
-}
+      {panels}
+    </Epic>
+  );
+};
 
 Navigation.propTypes = {
-    activePanel: PropTypes.string,
-    panelsData: PropTypes.array.isRequired,
-    onPanelChange: PropTypes.func,
-    fetchedUser: PropTypes.shape({
-      photo_200: PropTypes.string,
-      first_name: PropTypes.string,
-      last_name: PropTypes.string,
-    })
- }
-  
-Navigation.defaultProps = {
   activePanel: PropTypes.string,
-  activePanel: 'workflow',
+  panelsData: PropTypes.array.isRequired,
+  onPanelChange: PropTypes.func,
   fetchedUser: PropTypes.shape({
     photo_200: PropTypes.string,
     first_name: PropTypes.string,
     last_name: PropTypes.string,
-  })
+  }),
 };
 
-export default Navigation
+Navigation.defaultProps = {
+  activePanel: 'workflow',
+  onPanelChange: () => {},
+  fetchedUser: PropTypes.shape({
+    photo_200: PropTypes.string,
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+  }),
+};
+
+export default Navigation;
--- src/common.blocks/navigation/item.js
+++ src/common.blocks/navigation/item.js
@@ -1,13 +1,11 @@
 class Item {
-
-    constructor(onClick, selected, text, context) {
-        this.onClick = onClick;
-        this.selected = selected;
-        this.dataStory = dataStory;
-        this.text = text;
-        this.context = context;
-    }
-
+  constructor(onClick, selected, text, context) {
+    this.onClick = onClick;
+    this.selected = selected;
+    /* this.dataStory = dataStory; */
+    this.text = text;
+    this.context = context;
+  }
 }
 
-export default Item
+export default Item;
