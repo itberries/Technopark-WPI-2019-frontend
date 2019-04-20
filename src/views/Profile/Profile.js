8a641d978fbd38056d54d7143a69f9be61c60074
Anastasia Puchnina <puchnina.nastya@gmail.com>
2019-04-20 18:23:59 +0300

    update profile with loader and in background

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 import { View, Panel, PanelHeader } from '@vkontakte/vkui';
 
+import SpinnerCentered from '../../common.blocks/SpinnerCentered/SpinnerCentered';
 import User from '../../common.blocks/User/User';
+
+import { getUserProfile } from '../../actions/user';
 
 const mapStateToProps = (state) => {
   const { user } = state.user;
@@ -19,38 +23,79 @@ const mapStateToProps = (state) => {
     user.photo = state.vk.vkAppUser.vkUserInfo.photo_200;
   }
   return {
-    user: state.user.user,
+    user, // state.user.user,
   };
 };
 
-const Profile = ({ user, id }) => (
-  <View key={id} id={id} activePanel={id}>
-    <Panel id={id}>
-      <PanelHeader>Ваш профиль</PanelHeader>
-      <User user={user} />
-    </Panel>
-  </View>
+const mapDispatchToProps = dispatch => bindActionCreators(
+  {
+    getUserProfile,
+  },
+  dispatch,
 );
+
+/**
+ * Profile with user info and achievments
+ */
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      user: props.user,
+    };
+  }
+
+  async componentDidMount() {
+    if (typeof this.props.user !== 'undefined') {
+      await this.props.getUserProfile(this.props.user.id);
+      this.setState({
+        isLoading: false,
+      });
+    } else {
+      this.setState({
+        isLoading: true,
+      });
+    }
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (
+      prevState.isLoading === true
+      && typeof nextProps.user !== 'undefined'
+      && ((typeof prevState.user !== 'undefined' && prevState.user.score !== nextProps.user.score)
+        || prevState.user === undefined)
+    ) {
+      return {
+        ...prevState,
+        user: nextProps.user,
+        isLoading: false,
+      };
+    }
+    return {
+      ...prevState,
+    };
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    const { user, id } = this.props;
+    return (
+      <View key={id} id={id} activePanel={id}>
+        <Panel id={id} key={id}>
+          <PanelHeader>Ваш профиль</PanelHeader>
+          {isLoading ? <SpinnerCentered /> : <User user={user} />}
+        </Panel>
+      </View>
+    );
+  }
+}
 
 Profile.propTypes = {
   id: PropTypes.string.isRequired,
-  user: PropTypes.shape({
-    id: PropTypes.number,
-    photo: PropTypes.string,
-    firstName: PropTypes.string,
-    lastName: PropTypes.string,
-    score: PropTypes.number,
-  }),
 };
 
-Profile.defaultProps = {
-  user: PropTypes.shape({
-    id: PropTypes.number,
-    photo: PropTypes.string,
-    firstName: PropTypes.string,
-    lastName: PropTypes.string,
-    score: PropTypes.number,
-  }),
-};
-
-export default connect(mapStateToProps)(Profile);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Profile);
