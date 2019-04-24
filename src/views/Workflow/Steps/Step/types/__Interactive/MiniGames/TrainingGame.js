2de803b6c962526f3247610c209605716ea040a0
Ivan Nemshilov <i.nemshilov@corp.mail.ru>
2019-04-24 18:56:44 +0300

    renamed class TrainingGame.js

--- src/views/Workflow/Steps/Step/types/__Interactive/MiniGames/TrainingGame.js
+++ src/views/Workflow/Steps/Step/types/__Interactive/MiniGames/TrainingGame.js
@@ -3,10 +3,10 @@ import React from 'react';
 import MiniGame from './MiniGame';
 import MatchTraning from './Tranings/types/TraningMatch/TraningMatch';
 
-class InteractiveGame extends MiniGame {
+class TrainingGame extends MiniGame {
   generateMatch() {
     return <MatchTraning gameData={this.props.gameData} onComplete={this.props.onCompleted} />;
   }
 }
 
-export default InteractiveGame;
+export default TrainingGame;
