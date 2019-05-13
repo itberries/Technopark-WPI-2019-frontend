d795adaed79fb9600e92b053d5e3d478270f7288
Anastasia Puchnina <puchnina.nastya@gmail.com>
2019-05-13 20:18:14 +0300

    refactoing Leaderboard

--- src/views/Leaderboard/Leaderboard.js
+++ src/views/Leaderboard/Leaderboard.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import { View, Panel, PanelHeader } from '@vkontakte/vkui';
 
-const LeaderBoard = ({ id }) => (
-  <View key={id} id={id} activePanel={id}>
-    <Panel id={id}>
-      <PanelHeader>Таблица лидеров</PanelHeader>
-    </Panel>
-  </View>
-);
+class Leaderboard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-LeaderBoard.propTypes = {
+  render() {
+    return (
+      <View key={this.props.id} id={this.props.id} activePanel={this.props.id}>
+        <Panel id={this.props.id}>
+          <PanelHeader>Лидеры</PanelHeader>
+        </Panel>
+      </View>
+    );
+  }
+}
+
+Leaderboard.propTypes = {
   id: PropTypes.string.isRequired,
 };
 
-export default LeaderBoard;
+Leaderboard.defaultProps = {};
+
+export default Leaderboard;
