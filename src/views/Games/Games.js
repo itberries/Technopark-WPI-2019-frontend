de54efe3e5029935b01f94125a2c3a55a0dfd7a9
Anastasia Puchnina <puchnina.nastya@gmail.com>
2019-05-13 19:18:37 +0300

    move previews to GamesPreviews component

--- src/views/Games/Games.js
+++ src/views/Games/Games.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import {
-  Div, Group, View, Panel, PanelHeader, Header, Button,
-} from '@vkontakte/vkui';
+import { View, Panel, PanelHeader } from '@vkontakte/vkui';
 
+import GamesPreview from './GamesPreview/GamesPreview';
 import MultiplayerGame from './MultiplayerGame/MultiplayerGame';
-
-import './Games.scss';
-
-import rocket1Icon from '../../images/icons/Player1Rocket.svg';
-import rocket2Icon from '../../images/icons/Player2Rocket.svg';
-import academicCapIcon from '../../images/icons/academicCap.svg';
 
 class Games extends React.Component {
   constructor(props) {
@@ -19,58 +12,16 @@ class Games extends React.Component {
     this.state = {
       isGameStarted: false,
     };
-    this.stopGame = this.stopGame.bind(this);
+    this.onStopGame = this.onStopGame.bind(this);
+    this.onStartGame = this.onStartGame.bind(this);
   }
 
-  stopGame() {
-    console.log('GAMES END GAME');
+  onStopGame() {
     this.setState({ isGameStarted: false });
   }
 
-  generateGamesPreview() {
-    return (
-      <>
-        <Group className="game_preview">
-          <Header>Космо-гонки</Header>
-          <Div className="game_preview__icons">
-            <img className="game_preview__rocket1" src={rocket1Icon} alt="Ракета" />
-            <img className="game_preview__rocket2" src={rocket2Icon} alt="Ракета" />
-          </Div>
-          <Div className="game_preview__description">
-            Сразись в теоретической космо-гонке с другими учениками в IT галактике и докажи, что ты
-            лучший!
-          </Div>
-          <Button
-            className="game_preview__start_btn"
-            onClick={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
-              this.setState({ isGameStarted: true });
-            }}
-          >
-            Начать игру
-          </Button>
-        </Group>
-        <Group className="game_preview">
-          <Header>Своя IT-игра</Header>
-          <Div className="game_preview__icons">
-            <img
-              className="game_preview__academic_cap"
-              src={academicCapIcon}
-              alt="Квадратная академическая шапочка"
-            />
-          </Div>
-          <Div className="game_preview__description">
-            Вам предстоит проверить свою эрудицию и смекалку, отвечая на интересные вопросы
-            различной сложности и направленности. Противостоять в онлайн-битве будут другие знатоки
-            нашей галактики.
-          </Div>
-          <Button className="game_preview__start_btn game_preview__start_btn-unactive">
-            В разработке...
-          </Button>
-        </Group>
-      </>
-    );
+  onStartGame() {
+    this.setState({ isGameStarted: true });
   }
 
   render() {
@@ -79,9 +30,9 @@ class Games extends React.Component {
         <Panel id={this.props.id}>
           <PanelHeader>Онлайн-игры</PanelHeader>
           {!this.state.isGameStarted ? (
-            this.generateGamesPreview()
+            <GamesPreview onStartGame={this.onStartGame} />
           ) : (
-            <MultiplayerGame onEndGame={this.stopGame} />
+            <MultiplayerGame onEndGame={this.onStopGame} />
           )}
         </Panel>
       </View>
---
+++ src/views/Games/GamesPreview/GamesPreview.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  Group, Header, Div, Button,
+} from '@vkontakte/vkui';
+
+import './GamesPreview.scss';
+
+import rocket1Icon from '../../../images/icons/Player1Rocket.svg';
+import rocket2Icon from '../../../images/icons/Player2Rocket.svg';
+import academicCapIcon from '../../../images/icons/academicCap.svg';
+
+const GamesPreview = ({ onStartGame }) => (
+  <>
+    <Group className="game_preview">
+      <Header>Космо-гонки</Header>
+      <Div className="game_preview__icons">
+        <img className="game_preview__rocket1" src={rocket1Icon} alt="Ракета" />
+        <img className="game_preview__rocket2" src={rocket2Icon} alt="Ракета" />
+      </Div>
+      <Div className="game_preview__description">
+        Сразись в теоретической космо-гонке с другими учениками в IT галактике и докажи, что ты
+        лучший!
+      </Div>
+      <Button
+        className="game_preview__start_btn"
+        onClick={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          if (typeof onStartGame === 'function') {
+            onStartGame();
+          }
+        }}
+      >
+        Начать игру
+      </Button>
+    </Group>
+    <Group className="game_preview">
+      <Header>Своя IT-игра</Header>
+      <Div className="game_preview__icons">
+        <img
+          className="game_preview__academic_cap"
+          src={academicCapIcon}
+          alt="Квадратная академическая шапочка"
+        />
+      </Div>
+      <Div className="game_preview__description">
+        Вам предстоит проверить свою эрудицию и смекалку, отвечая на интересные вопросы различной
+        сложности и направленности. Противостоять в онлайн-битве будут другие знатоки нашей
+        галактики.
+      </Div>
+      <Button className="game_preview__start_btn game_preview__start_btn-unactive">
+        В разработке...
+      </Button>
+    </Group>
+  </>
+);
+
+GamesPreview.propTypes = {
+  onStartGame: PropTypes.func,
+};
+
+GamesPreview.defaultProps = {
+  onStartGame: undefined,
+};
+
+export default GamesPreview;
