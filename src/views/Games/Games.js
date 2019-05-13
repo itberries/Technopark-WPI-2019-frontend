ccb0360e1617f536514bc92ad4644187d93cefeb
Anastasia Puchnina <puchnina.nastya@gmail.com>
2019-05-13 18:40:53 +0300

    multiplayer screen style fixed, add second game preview, previews text

--- src/views/Games/Games.js
+++ src/views/Games/Games.js
@@ -26,33 +26,50 @@ class Games extends React.Component {
     this.setState({ isGameStarted: false });
   }
 
+  generateGamesPreview() {
+    return (
+      <>
+        <Group className="game">
+          <Header>Космические гонки</Header>
+          <Div className="game__rockets">
+            <img className="game__rocket1" src={rocket1Icon} alt="Ракета" />
+            <img className="game__rocket2" src={rocket2Icon} alt="Ракета" />
+          </Div>
+          <Div className="game__description">
+            Сразись в теоретической гонке с другими учениками в IT галактике и докажи, что ты
+            лучший!
+          </Div>
+          <Button
+            className="game__start_btn"
+            onClick={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              this.setState({ isGameStarted: true });
+            }}
+          >
+            Начать игру
+          </Button>
+        </Group>
+        <Group className="game">
+          <Header>Викторина IT</Header>
+          <Div className="game__description">
+            Вам предстоит проверить свою эрудицию и смекалку, отвечая на интересные вопросы
+            различной сложности и направленности. Противостоять в онлайн-битве будут другие знатоки
+            нашей галактики.
+          </Div>
+          <Button className="game__start_btn game__start_btn-unactive">В разработке...</Button>
+        </Group>
+      </>
+    );
+  }
+
   render() {
     return (
       <View key={this.props.id} id={this.props.id} activePanel={this.props.id}>
         <Panel id={this.props.id}>
           <PanelHeader>Онлайн-игры</PanelHeader>
           {!this.state.isGameStarted ? (
-            <Group className="game">
-              <Header>Космические гонки</Header>
-              <Div className="game__rockets">
-                <img className="game__rocket1" src={rocket1Icon} alt="Ракета" />
-                <img className="game__rocket2" src={rocket2Icon} alt="Ракета" />
-              </Div>
-              <Div className="game__description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore magna aliqua.
-              </Div>
-              <Button
-                className="game__start_btn"
-                onClick={(e) => {
-                  e.preventDefault();
-                  e.stopPropagation();
-                  this.setState({ isGameStarted: true });
-                }}
-              >
-                Начать игру
-              </Button>
-            </Group>
+            this.generateGamesPreview()
           ) : (
             <MultiplayerGame onEndGame={this.stopGame} />
           )}
